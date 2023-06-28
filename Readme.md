472fee8d2d5c771edb1f47be31238be1f8fc7534
erick-sudo <erickocchieng766@gmail.com>
2023-06-28 19:59:17 +0300

    Uppdate

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # Docker compose
 
 ![](compose-image.avif)
+
+
+```
+postgres:
+    build:
+      context: postgres-db
+      dockerfile: postgres-db.dockerfile
+```
---
+++ docker-front/src/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users").then((response) => {
+      if (response.status < 400) {
+        response.json().then((data) => {
+          setUsers(data);
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div className="bg-slate-900 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-24 pt-6 gap-4 px-4">
+      {users.map((user, index) => {
+        return (
+          <div key={index} className="bg-white rounded shadow-lg shadow-white border border-slate-900">
+            <div className="grid grid-cols-2 py-2 px-4 gap-4">
+              <h2 className="font-bold text-end">Id</h2>
+              <h2>{user.id}</h2>
+            </div>
+            <div className="grid grid-cols-2 py-2 px-4 gap-4">
+              <h2 className="font-bold text-end">Username</h2>
+              <h2>{user.username}</h2>
+            </div>
+            <div className="grid grid-cols-2 py-2 px-4 gap-4">
+              <h2 className="font-bold text-end">Email</h2>
+              <h2>{user.email}</h2>
+            </div>
+            <div className="grid grid-cols-2 py-2 px-4 gap-4">
+              <h2 className="font-bold text-end">Age</h2>
+              <h2>{user.age}</h2>
+            </div>
+            <div className="grid grid-cols-2 py-2 px-4 gap-4">
+              <h2 className="font-bold text-end">Gender</h2>
+              <h2>{user.gender}</h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
