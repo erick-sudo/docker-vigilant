dc33fbe838bb03c658ef52642f308db9ac4fff9c
erick-sudo <erickochieng766@gmail.com>
2023-06-29 13:03:14 +0300

    Migration

--- docker-api/db/seeds.rb
+++ docker-api/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+    User.create({
+        username: Faker::Name.name.downcase,
+        email: Faker::Internet.email,
+        age: rand(18..30),
+        gender: ["M", "F"].sample
+    })
+end
